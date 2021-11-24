3afabfc2e7073cd97f1a971e0d9e46c18ca0470d
Rafael Murad <rflmurad@gmail.com>
2021-11-23 22:38:56 -0300

    sign up page forms

--- src/pages/create-user/index.tsx
+++ src/pages/create-user/index.tsx
@@ -2,14 +2,41 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import { CreateUser as Create } from 'services/user';
+import { CreateUser as Create, GetUser } from 'services/user';
+import { Spinner } from 'react-bootstrap';
 
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Actions from 'shared/actions';
 
 import './index.scss';
 
 const CreateUser = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [age, setAge] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const createUser = (event: any) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    console.log(name, email, password, age);
+
+    // Create(name, email, password, age).then((id) => {
+    //   GetUser(id).then((result) => {
+    //     dispatch(Actions.setUser(result));
+    //   });
+
+    //   localStorage.setItem('userId', id.toString());
+    //   setIsLoading(false);
+    //   navigate('/');
+    // });
+  };
 
   return (
     <div className="container text-center">
@@ -17,25 +44,25 @@ const CreateUser = () => {
       <div className="row">
         <h3>Sign up</h3>
       </div>
-      <Form onSubmit={() => navigate('/dashboard')} className="form pt-4">
+      <Form onSubmit={createUser} className="form pt-4">
         <Form.Group controlId="name">
           <FloatingLabel controlId="floatingInput" label="Name" className="mb-4">
-            <Form.Control type="name" placeholder="Please enter your name" />
-          </FloatingLabel>
-        </Form.Group>
-        <Form.Group controlId="surname">
-          <FloatingLabel controlId="floatingInput" label="Surname" className="mb-4">
-            <Form.Control type="surname" placeholder="Please enter your surname" />
+            <Form.Control onChange={(change) => setName(change.target.value)} value={name} type="name" placeholder="Please enter your name" />
           </FloatingLabel>
         </Form.Group>
         <Form.Group controlId="email">
           <FloatingLabel controlId="floatingInput" label="Email address" className="mb-4">
-            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Control onChange={(change) => setEmail(change.target.value)} value={email} type="email" placeholder="name@example.com" />
           </FloatingLabel>
         </Form.Group>
         <Form.Group controlId="password">
           <FloatingLabel controlId="floatingPassword" label="Password" className="mb-4">
-            <Form.Control type="password" placeholder="name@example.com" />
+            <Form.Control onChange={(change) => setPassword(change.target.value)} value={password} type="password" placeholder="Password must have min. 8 digits" />
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group controlId="age">
+          <FloatingLabel controlId="floatingInput" label="Age" className="mb-4">
+            <Form.Control onChange={(change) => setAge(parseInt(change.target.value, 10))} value={age} type="number" placeholder="Enter your age" />
           </FloatingLabel>
         </Form.Group>
 
